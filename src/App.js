3be17a4fd9fa18d707964ba2bf76dc1ce5ce7449
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-12-27 23:02:52 +0100

    Setup the project and add the basic components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Inventory from "./components/Inventory";
+import Order from "./components/Order";
+
+function App() {
+  return (
+    <div className='catch-of-the-day'>
+      <div className='menu'>
+        <Header tagline={"Fresh Seafood Market"} />
+      </div>
+      <Order />
+      <Inventory />
+    </div>
+  );
+}
+
+export default App;
